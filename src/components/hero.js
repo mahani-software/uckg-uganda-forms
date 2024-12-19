35af06d1f03039257ab7b28a028fc803e53f8775
Rodgers Mahanitech <mahanitech@gmail.com>
2024-12-19 20:12:14 +0300

    change the bg image

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import headerImage from '../images/img009.png';
+import bannerImage from "../images/bgimage01.webp";
 import logoImage from '../images/logo-advert-transparent2.png';
 
 const Hero = () => {
+
     return (
         <div
             id="home"
-            //className="flex relative z-10 overflow-hidden lg:flex justify-between lg:flex-row lg:gap-2"
-            className={`flex relative z-10 overflow-hidden lg:flex justify-between lg:flex-row lg:gap-2 bg-[url(${headerImage})] bg-[100%] lg:bg-[75%] bg-no-repeat mt-[70px]`}
-        // style={{
-        //     backgroundImage: `url(${headerImage})`,
-        //     backgroundSize: '75%',
-        //     backgroundRepeat: 'no-repeat',
-        //     marginTop: '70px'
-        // }}
+            style={{ backgroundImage: `url(${bannerImage})`, backgroundRepeat: "no-repeat", backgroundSize: "100%", backgroundPositionY: "0", backgroundPositionX: "0" }}
+            className={`flex relative z-10 lg:flex justify-between flex-col lg:flex-row lg:gap-2 bg-no-repeat mt-[70px] bg-[100%] lg:bg-[75%]`}
         >
-            <div className="lg:w-[40%] mt-200">
+            <div className="w-full lg:w-[40%] mt-200">
                 <div className="w-full p-[5%] pr-0 items-center">
                     <div
                         className="p-[5%] rounded w-full pt-100"
@@ -29,17 +24,19 @@ const Hero = () => {
                             Networking, Cloud computing, Blockchain development,
                             systems analysis and design, and more.
                         </p>
+                        {/* 
                         <div className="hero_btn mt-10">
                             <a className="main-btn" href="#work"><b>See our projects</b></a>
-                        </div>
+                        </div> 
+                        */}
                     </div>
                 </div>
             </div>
 
-            <div className="relative items-center pt-[50px] overflow-hidden p-8 lg:w-[50%]">
-                <div class="absolute inset-0 bg-blue-50 transform -skew-x-12 z-0 ml-[10%] w-[200%]"></div>
+            <div className="relative items-center pt-[50px] overflow-hidden p-8 pr-0 w-full lg:w-[50%]">
+                <div class="absolute inset-0 bg-blue-50 transform -skew-x-[15deg] z-0 ml-[10%] w-[200%]"></div>
                 <div className="relative image z-50 pl-[10%]">
-                    <img src={logoImage} alt="Header Image" />
+                    <img src={logoImage} alt="" />
                 </div>
             </div>
         </div>
